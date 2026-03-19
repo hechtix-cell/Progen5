9ad1d041c2c5d7d620e4e9dfd95c325f22f368b1
vivek-nairy <viveknairy0@gmail.com>
2026-03-19 20:22:33 +0530

    chat

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
-import ChatWidget from './components/ChatWidget';
+import ChatWidget from '../components/ChatWidget';
 
 const inter = Inter({
   subsets: ["latin"],
